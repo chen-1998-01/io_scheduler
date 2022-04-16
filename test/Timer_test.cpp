58bb04adf44ce0383baa26223546caf9d0558f6b
platform <3323512326@qq.com>
2022-04-16 10:11:31 -0700

    formal

--- test/Timer_test.cpp
+++
@@ -1,8 +0,0 @@
-#include"../include/timer.h"
-#include<iostream>
-
-int main(int argc,char* argv[]){
-    Timer_manager::timers_ptr timers_test(new Timer_manager());
-    std::cout<<timers_test->GetNextTime()<<std::endl;
-    return 0;
-}
